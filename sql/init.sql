bb6063aad4d51470399066252cf9b6efcf39d45d
wass80 <wasss80@gmail.com>
2023-11-25 12:43:02 +0900

    not need trancate

--- sql/init.sql
+++ sql/init.sql
@@ -1,15 +1,15 @@
-TRUNCATE TABLE themes;
-TRUNCATE TABLE icons;
-TRUNCATE TABLE reservation_slots;
-TRUNCATE TABLE livestream_viewers_history;
-TRUNCATE TABLE livecomment_reports;
-TRUNCATE TABLE ng_words;
-TRUNCATE TABLE reactions;
-TRUNCATE TABLE tags;
-TRUNCATE TABLE livestream_tags;
-TRUNCATE TABLE livecomments;
-TRUNCATE TABLE livestreams;
-TRUNCATE TABLE users;
+-- TRUNCATE TABLE themes;
+-- TRUNCATE TABLE icons;
+-- TRUNCATE TABLE reservation_slots;
+-- TRUNCATE TABLE livestream_viewers_history;
+-- TRUNCATE TABLE livecomment_reports;
+-- TRUNCATE TABLE ng_words;
+-- TRUNCATE TABLE reactions;
+-- TRUNCATE TABLE tags;
+-- TRUNCATE TABLE livestream_tags;
+-- TRUNCATE TABLE livecomments;
+-- TRUNCATE TABLE livestreams;
+-- TRUNCATE TABLE users;
 
 ALTER TABLE `themes` auto_increment = 1;
 ALTER TABLE `icons` auto_increment = 1;
