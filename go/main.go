263b73a9094afadd8ac5e6f7303b60034ac569ad
utgwkk <utagawakiki@gmail.com>
2023-11-28 00:48:13 +0900

    Revert "initializeでiconのキャッシュがexpireするのを待つ"
    This reverts commit 372d606124ba728fea06458dfba4ebfdc8113d53.
    2秒で更新されないとだめだった

--- go/main.go
+++ go/main.go
@@ -13,7 +13,6 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
-	"time"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -135,9 +134,6 @@ func initializeHandler(c echo.Context) error {
 		c.Logger().Warnf("init.sh failed with err=%s", string(out))
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to initialize: "+err.Error())
 	}
-
-	// iconのキャッシュがexpireするのを待つ
-	time.Sleep(10 * time.Second)
 
 	c.Request().Header.Add("Content-Type", "application/json;charset=utf-8")
 	return c.JSON(http.StatusOK, InitializeResponse{
