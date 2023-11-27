eab6c8b20401f5774d68bbc666177217aa64f71f
utgwkk <utagawakiki@gmail.com>
2023-11-28 01:40:59 +0900

    ORDER BY狙い

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -85,7 +85,7 @@ CREATE TABLE `livecomments` (
   `created_at` BIGINT NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 ALTER TABLE `livecomments` ADD KEY `livecomments_user_id` (`user_id`);
-ALTER TABLE `livecomments` ADD KEY `livecomments_livestream_id` (`livestream_id`);
+ALTER TABLE `livecomments` ADD KEY `livecomments_livestream_id_created_at_desc` (`livestream_id`, `created_at` DESC);
 
 -- ユーザからのライブコメントのスパム報告
 CREATE TABLE `livecomment_reports` (
