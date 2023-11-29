2fb002292c95d3c19d7ebfb806d3d9fc6530632c
utgwkk <utagawakiki@gmail.com>
2023-11-29 20:36:53 +0900

    初期データがおかしい

--- sql/initial_countercache.sql
+++ sql/initial_countercache.sql
@@ -1,1 +1,2 @@
-INSERT INTO reaction_per_livestream SELECT l.id AS livestream_id, COUNT(*) AS reaction_count FROM livestreams l LEFT JOIN reactions r ON l.id = r.livestream_id GROUP BY l.id;
+INSERT INTO reaction_per_livestream
+SELECT l.id AS livestream_id, COUNT(r.id) AS reaction_count FROM livestreams l LEFT JOIN reactions r ON l.id = r.livestream_id GROUP BY l.id;
