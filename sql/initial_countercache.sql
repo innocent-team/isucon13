64848a5cebf50db055f3ea9168c147e5404dbc01
utgwkk <utagawakiki@gmail.com>
2023-11-29 20:25:15 +0900

    oops

--- sql/initial_countercache.sql
+++ sql/initial_countercache.sql
@@ -1,2 +1,1 @@
-INSERT INTO reaction_per_livestream SELECT l.id AS livestream_id, COUNT(*) AS reaction_count FROM livestreams l L
-EFT JOIN reactions r ON l.id = r.livestream_id GROUP BY l.id;
+INSERT INTO reaction_per_livestream SELECT l.id AS livestream_id, COUNT(*) AS reaction_count FROM livestreams l LEFT JOIN reactions r ON l.id = r.livestream_id GROUP BY l.id;
